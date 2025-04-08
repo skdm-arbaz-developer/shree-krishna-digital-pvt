a00078fd6057bb24ed0ed537deb3ee3c9f926f1a
skdm-arbaz-developer <129583593+skdm-arbaz-developer@users.noreply.github.com>
2025-04-08 18:13:20 +0530

    bug fixed

--- src/components/layout/DropDown.tsx
+++ src/components/layout/DropDown.tsx
@@ -1,20 +1,9 @@
 import { ConfigProvider, Popover } from "antd";
 import Comopnay from "./DropdownLayout/Comopnay";
-import { ReactNode } from "react";
 import ServicesLayout from "./DropdownLayout/ServicesLayout";
 
-interface DropDownProps {
-  nav: string;
-  links: Array<{
-    link: string;
-    name: string;
-    desc: string;
-    logos: Array<string>;
-  }>; // Corrected type
-  children?: ReactNode; // Allow children
-}
 
-export default function DropDown({ nav, links, children }: DropDownProps) {
+export default function DropDown({ nav, links, children }: any) {
   return (
     <ConfigProvider
       theme={{
