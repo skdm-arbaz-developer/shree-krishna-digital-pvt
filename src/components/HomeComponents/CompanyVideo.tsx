a18a482e6611327dc81b5d462051812ce1c9de40
skdm-arbaz-developer <129583593+skdm-arbaz-developer@users.noreply.github.com>
2025-05-06 16:15:56 +0530

    bug fixed

--- src/components/HomeComponents/CompanyVideo.tsx
+++ src/components/HomeComponents/CompanyVideo.tsx
@@ -4,15 +4,14 @@ export default function CompanyVideo() {
   return (
     <div className="container mx-auto mt-15 mb-15">
       <iframe
-        width={"100%"}
-        height={"550"}
-        src="https://www.youtube-nocookie.com/embed/r1uKGjwrpZk?si=KMUv0tK_Ftf4l3W2&amp;controls=0"
+        width='{"100%"}'
+        height='{"550"}'
+        src="https://www.youtube-nocookie.com/embed/r1uKGjwrpZk?si=KMUv0tK_Ftf4l3W2&controls=0"
         title="YouTube video player"
-        frameborder="0"
+        frameBorder={0}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
+        referrerPolicy="strict-origin-when-cross-origin"
+      />
     </div>
   );
 }
