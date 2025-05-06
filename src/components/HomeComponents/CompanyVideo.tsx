ff1c3e2161492bbdc0afda06395ebecc0c0bb08c
skdm-arbaz-developer <129583593+skdm-arbaz-developer@users.noreply.github.com>
2025-05-06 16:22:44 +0530

    resolve issue

--- src/components/HomeComponents/CompanyVideo.tsx
+++ src/components/HomeComponents/CompanyVideo.tsx
@@ -2,8 +2,8 @@ export default function CompanyVideo() {
   return (
     <div className="container mx-auto mt-15 mb-15">
       <iframe
-        width='{"100%"}'
-        height='{"550"}'
+        width={"100%"}
+        height={"550"}
         src="https://www.youtube-nocookie.com/embed/r1uKGjwrpZk?si=KMUv0tK_Ftf4l3W2&controls=0"
         title="YouTube video player"
         frameBorder={0}
