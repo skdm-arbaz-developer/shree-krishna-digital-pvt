9121265c17159147737ee15bef95fa124da66f16
skdm-arbaz-developer <129583593+skdm-arbaz-developer@users.noreply.github.com>
2025-05-30 19:34:56 +0530

    new update

--- src/components/CountryLandingPage/Testimonial.tsx
+++ src/components/CountryLandingPage/Testimonial.tsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Testimonial({ testimonialRef }: any) {
   return (
+    // 
     <section className="testimonials-section" ref={testimonialRef}>
       <div className="container mx-auto">
         <div className="sec-title">
